7008fe20b3f0e5cf048b8d3a8ae39020ed09fb29
liukunyu <444736325@qq.com>
2022-09-21 16:55:10 +0800

    1

---
+++ src/main/java/com/gitdemo/controller/HelloGit.java
@@ -0,0 +1,15 @@
+package com.gitdemo.controller;
+
+/**
+ * @author 86185
+ * @ClassName HelloGit
+ * @Description TODO
+ * @date 2022/9/21 16:30
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git1");
+        System.out.println("HelloGit");
+    }
+}
